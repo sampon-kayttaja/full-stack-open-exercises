3c35b74c38b04a4175f978cffe40de4b92ccc2be
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-05-18 19:17:24 +0300

    part1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,41 +7,39 @@ const Statistics = (props) => {
   if (props.all === 0) 
     return <p>No feedback given</p>
   return (<div>
-    <StatisticsLine text="good" value={props.good} />
-    <StatisticsLine text="neutral" value={props.neutral} />
-    <StatisticsLine text="bad" value={props.bad} />
-    <StatisticsLine text="all" value={props.all} />
-    <StatisticsLine text="average" value={props.average} />
-    <StatisticsLine text="positive" value={props.positivePercentage} />
+    <h1>statistics</h1>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={props.good} />
+        <StatisticsLine text="neutral" value={props.neutral} />
+        <StatisticsLine text="bad" value={props.bad} />
+        <StatisticsLine text="all" value={props.all} />
+        <StatisticsLine text="average" value={props.average} />
+        <StatisticsLine text="positive" value={props.positivePercentage} />
+      </tbody>
+    </table>
   </div>
   )
 }
 
-const StatisticsLine = ({ text, value}) => <p>{text} {value}</p>
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    console.log('good clicked')
-  }
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    console.log('neutral clicked')
-  }
-
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    console.log('bad clicked')
-  }
-
-  const all = () => {
-    return good + neutral + bad
-  }
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+  const all = () =>  good + neutral + bad
 
   const average = () => {
     const total = all()
