da411dd57cf9c979c31b4e33853fc949651b983f
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-06-03 19:22:09 +0300

    3.7, 3.8

--- part3/puhelinluettelobackend/index.js
+++ part3/puhelinluettelobackend/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+}))
 
 let persons = [
   {
@@ -83,7 +94,6 @@ app.post('/api/persons', (req, res) => {
   }
 
   persons = persons.concat(person)
-
   res.json(person)
 })
 
