b31b68f03059be74423e97039313ae76bec7ef3f
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-06-03 19:07:54 +0300

    3.4, 3.5, 3.6

--- part3/puhelinluettelobackend/index.js
+++ part3/puhelinluettelobackend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 let persons = [
   {
@@ -46,6 +47,44 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000000)
+  return randomId
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({ error: 'name is missing' })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({ error: 'number is missing' })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 const PORT = process.env.PORT || 3001
