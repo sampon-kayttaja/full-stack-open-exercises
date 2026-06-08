48c296d248843254f1bd7b6aa5d64d9cfd6a21f1
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-06-09 00:11:24 +0300

    3.22

--- part3/puhelinluettelobackend/models/person.js
+++ part3/puhelinluettelobackend/models/person.js
@@ -3,23 +3,24 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console
-.log('connecting to', url)
+
+console.log('connecting to', url)
+
 mongoose.connect(url, { family: 4 })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 const personSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    minlength: [3, 'name must be at least 3 characters long'], 
-    required: [true, 'name is required'] 
+  name: {
+    type: String,
+    minlength: [3, 'name must be at least 3 characters long'],
+    required: [true, 'name is required']
   },
-  number: { 
-    type: String,  
+  number: {
+    type: String,
     validate: {
       validator: function(v) {
         return /^\d{2,3}-\d{6,15}$/.test(v)
