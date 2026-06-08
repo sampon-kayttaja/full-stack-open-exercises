61af374fa81bcf418e74865b6b96df6467693ec6
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-06-08 14:09:44 +0300

    part 3.11

--- part3/puhelinluettelobackend/index.js
+++ part3/puhelinluettelobackend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const cors = require('cors')
 var morgan = require('morgan')
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
