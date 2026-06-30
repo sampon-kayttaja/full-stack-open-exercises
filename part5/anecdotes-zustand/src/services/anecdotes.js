3e2c927731cd95221f50c9824f3297ce638a3008
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-06-30 18:39:30 +0300

    6.7

---
+++ part5/anecdotes-zustand/src/services/anecdotes.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch notes')
+  }
+
+  return await response.json()
+}
+
+
+
+
+export default { getAll }
--- part5/anecdotes-zustand/src/store.js
+++ part5/anecdotes-zustand/src/store.js
@@ -1,14 +1,5 @@
-
 import { create } from 'zustand'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import anecdoteService from './services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -19,7 +10,7 @@ const asObject = anecdote => ({
 })
 
 const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
   filter: '',
   actions: {
     add: anecdote => set(
@@ -32,7 +23,11 @@ const useAnecdoteStore = create((set) => ({
         )
       })
     ),
-    setFilter: value => set(() => ({ filter: value }))
+    setFilter: value => set(() => ({ filter: value })),
+    initialize: async () => {
+      const anecdotes = await anecdoteService.getAll()  
+      set ({anecdotes})
+    }
   }
 }))
 
