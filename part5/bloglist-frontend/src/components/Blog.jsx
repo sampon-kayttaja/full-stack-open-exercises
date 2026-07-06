811d36362e05bc710712da9355e3d5ec0fee68e9
Sampo Toijonen <sadtoijo@cubbli24-gold.cs.helsinki.fi>
2026-07-06 21:03:02 +0300

    5.7.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+    <div style={blogStyle}>
+      {!expanded && (
+        <div>
+          {blog.title} {blog.author} <button onClick={() => setExpanded(true)}>view</button>
+        </div>
+      )}
+      {expanded && (
+        <div>
+          {blog.title} {blog.author} <button onClick={() => setExpanded(false)}>hide</button> <br />
+          {blog.url} <br/>
+          {blog.likes} <button>like</button> <br/>
+          {blog.user.username}
+        </div>
+      )}  
+    </div>   
+  )
+}
 
 export default Blog
